c772f3bb9ac12e75b358c7b592df09ea6ce919fa
Evgeny_Goryunov <ges987@list.ru>
2022-04-07 11:44:10 +0700

    .старт и финиш

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    static class Person {
+        int id;
+        String name;
+
+        public Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    public static class PersonBuilder {
+        private static Person buildPerson(int id, String name) {
+            return new Person(id, name);
+        }
+
+        public static void main(String[] args) {
+            int id = 23;
+            String name = "John";
+            Person person = null;
+            person = buildPerson(id, name);
+        }
+    }
+}
+
